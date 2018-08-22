b1b689cd7e77c15953fb6e51806b4949f19c073f
Tyler Durham <tyler@tylers-imac.lan>
2018-08-22 15:01:20 -0500

    Basic Functionality
    Ported to Azure Web Function

---
+++ messages/db.js
@@ -0,0 +1,55 @@
+
+const sql = require('mssql')
+
+function parseKeywords(keyword) {
+    var regex = /("(.*)")|[^\W\d]+[\u00C0-\u017Fa-zA-Z'](\w|[-'](?=\w))*("(.*)")|[^\W\d]+[\u00C0-\u017Fa-zA-Z'](\w|[-'](?=\w))*/gi;
+    var tokens = keyword.match(regex);
+    return tokens.join (' AND ');
+}
+
+const config = {
+    user: process.env.DB_UID,
+    password: process.env.DB_PWD,
+    server: process.env.DB_SERVER, // You can use 'localhost\\instance' to connect to named instance
+    database: process.env.DB_NAME,
+ 
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+}
+
+const mappings = { 
+    'ICD10': 'code',
+     'Short_Description': "description",
+     "ChapterCodeKey": 'chapter',
+     'HIPAA_valid': 'hippa'
+}
+
+exports.searchCodes = function(keywords) {   
+    
+    return new Promise((resolve, reject) => {
+        var results = [];
+
+        const pool = new sql.ConnectionPool(config, err => {
+            if(err) reject(err);
+
+            pool.request()
+                    .input('KEYWORDS', sql.VarChar, parseKeywords(keywords))
+                    .execute('[dbo].[SEARCH_CODES]')
+                    .then((data) => {
+                        data.recordset.forEach((record) => {
+                            results.push({
+                                "code": record["ICD10"].trim(),
+                                "description": record["Short_Description"].trim(),
+                                "hipaa": record["HIPAA_valid"]
+                            });
+                        });
+                        resolve(results);
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    })
+        });
+        
+    })
+}
--- messages/index.js
+++ messages/index.js
@@ -1,20 +1,12 @@
 'use strict';
-
-var builder = require('botbuilder');
+require('dotenv').config();
+const builder = require('botbuilder');
 const helpDialog = require('./dialogs/help').dialogInfo;
 const searchCodesDialog = require('./dialogs/searchCodes').dialogInfo;
 
 var connector = new builder.ChatConnector({
     appId: process.env.CHAT_CONNECTOR_APP_ID,
     appPassword: process.env.CHAT_CONNECTOR_APP_PASSWORD
-});
-
-var LUIS_RECOGNIZER_URL = process.env.LUIS_RECOGNIZER_URL;
-
-var recognizers = [];
-
-var intents = new builder.IntentDialog({
-    recognizers: recognizers
 });
 
 var bot = new builder.UniversalBot(connector);
